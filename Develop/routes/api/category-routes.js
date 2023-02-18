9004d727b0e017cc4911624fd88ec3e5ba51a385
Jonathan Ray <jonathanjudsonray@gmail.com>
2023-02-17 20:33:17 -0500

    Completed the routes files.

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
-
 router.get('/', (req, res) => {
   // find all categories
   Category.findAll({
@@ -32,16 +30,42 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
-  // To pull out info...req.body
-  //.create{keyValue Pair} {name: req.body.name}
+  Category.create({
+    category_name: req.body.category_name 
+  }).then(newCategory => {
+    res.json(newCategory);
+  }).catch(err => {
+    console.log(err);
+    res.json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  }).then(updatedCategory => {
+    res.json(updatedCategory);
+  }).catch(err => {
+    console.log(err);
+    res.json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(deletedCategory => {
+    res.json(deletedCategory);
+  }).catch(err => {
+    console.log(err);
+    res.json(err);
+  });
 });
 
 module.exports = router;
